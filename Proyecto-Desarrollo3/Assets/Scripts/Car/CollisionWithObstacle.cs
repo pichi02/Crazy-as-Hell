39de2ef61248f1cb3ae302a8f798bccd53680385
Matias <matiasmonti2011@hotmail.com>
2022-10-31 16:20:57 -0300

    Add particles collision car with powerup

--- Proyecto-Desarrollo3/Assets/Scripts/Car/CollisionWithObstacle.cs
+++ Proyecto-Desarrollo3/Assets/Scripts/Car/CollisionWithObstacle.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class CollisionWithObstacle : MonoBehaviour
 {
     [SerializeField] private ParticleSystem particleCollision;
+    [SerializeField] private ParticleSystem particleCollisionPowerup;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("Obstacle") || collision.transform.CompareTag("BearTrap"))
         {
             particleCollision.Play();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("PowerUp"))
+        {
+            particleCollisionPowerup.Play();
         }
     }
 }
