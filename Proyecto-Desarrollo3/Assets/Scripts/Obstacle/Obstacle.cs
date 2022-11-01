a49d8a5b2dc80083cbb1091494a9422f015989af
pichi02 <lucas.pich02@gmail.com>
2022-11-01 10:53:25 -0300

    Add particle when obstacle is instantiated

--- Proyecto-Desarrollo3/Assets/Scripts/Obstacle/Obstacle.cs
+++ Proyecto-Desarrollo3/Assets/Scripts/Obstacle/Obstacle.cs
@@ -13,9 +13,11 @@ public class Obstacle : MonoBehaviour
 
     [SerializeField] private float maxTime = 3;
     [SerializeField] private int damage;
+    [SerializeField] private ParticleSystem particle;
 
     private void Start()
     {
+        particle.Play();
         Destroy(gameObject, maxTime);
     }
     private void OnCollisionEnter(Collision collision)
@@ -32,5 +34,5 @@ public class Obstacle : MonoBehaviour
         }
     }
 
-  
+
 }
